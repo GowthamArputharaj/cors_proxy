8dfb9c23e8c7e2a8578f664b4d96f6f20b125534
GowthamArputharaj <gowtham.amateur.id@gmail.com>
2021-03-23 12:28:52 +0530

    package start

--- cors.js
+++ cors.js
@@ -1,3 +1,4 @@
+var cors_proxy = require('cors-anywhere');
 // Listen on a specific host via the HOST environment variable
 // var host = process.env.HOST || '0.0.0.0';
 // var host = 'https://gowthamarputharaj.github.io/cors_proxy/';
@@ -9,19 +10,22 @@
 // var host = getHost();
 var host = '0.0.0.0'
 var port = 8888;
+require('cors-anywhere', (cors_proxy) => {
+    cors_proxy.createServer({
+        originWhitelist: [], // Allow all origins
+        // requireHeader: ['origin', 'x-requested-with'],
+        removeHeaders: ['cookie', 'cookie2']
+    }).listen(port, host, function() {
+        console.log('Running CORS Anywhere on ' + host + ':' + port);
+    });
+    
+    fetch('https://localhost:8888/https://www.google.com').then(e => console.log(e)).catch(e => console.log(e.message));
 
-var cors_proxy = require('cors-anywhere');
-cors_proxy.createServer({
-    originWhitelist: [], // Allow all origins
-    // requireHeader: ['origin', 'x-requested-with'],
-    removeHeaders: ['cookie', 'cookie2']
-}).listen(port, host, function() {
-    console.log('Running CORS Anywhere on ' + host + ':' + port);
-});
+    fetch('http://localhost:8888/https://www.google.com').then(e => console.log(e)).catch(e => console.log(e.message));
+
+})
 
 
-fetch('https://localhost:8888/https://www.google.com').then(e => console.log(e)).catch(e => console.log(e.message));
 
-fetch('http://localhost:8888/https://www.google.com').then(e => console.log(e)).catch(e => console.log(e.message));
 
 
